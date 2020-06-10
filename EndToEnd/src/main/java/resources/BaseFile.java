9c8c2ad531308a47e8d4f37cb633335d71f88b5c
Rohit Khavanekar <rohit.oyc9@gmail.com>
2020-06-10 14:34:58 +0530

    6-10-2020

--- EndToEnd/src/main/java/resources/BaseFile.java
+++ EndToEnd/src/main/java/resources/BaseFile.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseFile   //THIS FILE IS BASE FILE ONLY CHANGE THE PARAMETRS IN THIS FILE 
@@ -21,22 +22,29 @@ public class BaseFile   //THIS FILE IS BASE FILE ONLY CHANGE THE PARAMETRS IN TH
 	public WebDriver initlizeDriver() throws IOException
 	{
 		Properties prop = new Properties();
-		FileInputStream fis = new FileInputStream("C:\\Users\\Dell\\eclipse-workspace\\E2EProject\\src\\main\\java\\resources\\data.properties");
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") +"\\src\\main\\java\\resources\\data.properties");
 		
 		prop.load(fis);
-		//String browserName = System.getProperty("browser");       FOR JENKINS PARAMETRIZED WORKING
-		String browserName = prop.getProperty("browser");
+		String browserName = System.getProperty("browser");// FOR JENKINS PARAMETRIZED WORKING
+		//String browserName = prop.getProperty("browser");
 		
 		
 		
 		if(browserName.equalsIgnoreCase("Chrome"))//USE EQUALS OPERATOR CASE
 		{
-			System.setProperty("webdriver.chrome.driver", "D:\\Rohit_Folder\\MAXIMUS\\Selenium Driver NEVER DELETE\\ChromeDriver 83\\chromedriver.exe");
+			
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\java\\resources\\chromedriver.exe"); //Dynamic Paths
+			//ChromeOptions options = new ChromeOptions();//FOR HEADLESS RUNNING OF CHROME  ----1
+			//options.addArguments("--headless");//FOR HEADLESS RUNNING OF CHROME  ----2
+			//System.setProperty("webdriver.chrome.driver", "D:\\Rohit_Folder\\MAXIMUS\\Selenium Driver NEVER DELETE\\ChromeDriver 83\\chromedriver.exe");
+			//driver = new ChromeDriver(options);//FOR HEADLESS RUNNING OF CHROME  ----3
 			driver = new ChromeDriver();
 		}
+		
 		else if(browserName.equalsIgnoreCase("Firefox"))
 		{
-			System.setProperty("webdriver.chrome.driver", "D:\\Rohit_Folder\\MAXIMUS\\SELENIUM\\GeckoDriver\\geckodriver");
+			System.setProperty("webdriver.firefox.driver", System.getProperty("user.dir")+"\\src\\main\\java\\resources\\geckodriver.exe");//Dynamic Paths
+			//System.setProperty("webdriver.firefox.driver", "D:\\Rohit_Folder\\MAXIMUS\\SELENIUM\\GeckoDriver\\geckodriver");
 			driver = new FirefoxDriver();
 		}
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
